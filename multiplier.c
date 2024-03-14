52bf3bacf82b12f71bc90eddb872d7f90e12801b
Lucien Van Bussel <lvan-bus@f1r6s8.codam.nl>
2024-03-14 13:46:17 +0100

    functional

--- multiplier.c
+++ multiplier.c
@@ -1,82 +1,57 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
 
-int	main(int argc, char **argv)
-{
-	if (argc != 3)
-	{
-		printf("incorrect number of arguments\n");
-		return 1;
-	}
-	char	*s1 = argv[1];
-	char	*s2 = argv[2];
-	int		l1 = strlen(s1) -1;
-	int		l2 = strlen(s2) -1;
-	int		l2_start = l2;
-	int		len = l1 + l2 + 2;
-	int		*arr = malloc(len * sizeof(int));
-	int		*res = malloc(len * sizeof(int));
-	int		i = 0;
-	int		count = 0;
+int char_to_digit(char c) {
+    return c - '0';
+}
 
-	while (l1 >= 0)
-	{
-		i = count;
-		if (l2 >= 0)
-		{
-			while (l2 >= 0)
-			{
-				arr[i] += (s1[l1] - '0') * (s2[l2] - '0');
-				l2--;
-				i++;
-			}
-		}
-		l2 = l2_start;
-		count++;
-		l1--;
-	}
+char digit_to_char(int digit) {
+    return digit + '0';
+}
 
-	printf("i: %i\n", i);
-	int	y = i -1;
-	int	keep = 0;
-	int	k = 0;
-	while (y >= 0)
-	{
-		if (arr[y] > 9)
-		{
-			res[k] = (arr[y] + keep) % 10;
-			keep = (arr[y] + keep) / 10;
-		}
-		else
-		{
-			res[k] = arr[y] + keep;
-			keep = 0;
-		}
-		printf("res[%i]: %i, keep: %i\n", y, res[k], keep);
-		y--;
-		k++;
-	}
-	if (keep != 0)
-		res[k] = keep;
-	
-	
-	printf("____________\n\n");
-	y = 0;
-	while (y < i)
-	{
-		printf("arr[%i], %i\n", y, arr[y]);
-		y++;
-	}
-	y = k;
-	while (y >= 0)
-	{
-		printf("%i", res[y]);
-		y--;
-	}
-	printf("\n");
-	free(arr);
-	free(res);
-	return 0;
+void multiply_strings(const char *str1, const char *str2) {
+
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+    int len_result = len1 + len2;
+
+    int *result = (int *)malloc(len_result * sizeof(int));
+	int	i = 0;
+	while (i < len_result)
+		result[i++] = 0;
+
+    int is_negative = (str1[0] == '-' && str2[0] != '-') || (str1[0] != '-' && str2[0] == '-');
+    if (is_negative) {
+        printf("-");
+    }
+
+    for (int i = len1 - 1; i >= (str1[0] == '-'); i--) {
+        for (int j = len2 - 1; j >= (str2[0] == '-'); j--) {
+            int mul = char_to_digit(str1[i]) * char_to_digit(str2[j]);
+            int sum = mul + result[i + j + 1];
+            result[i + j + 1] = sum % 10;
+            result[i + j] += sum / 10;
+        }
+    }
+
+    int start_index = 0;
+    while (start_index < len_result && result[start_index] == 0) {
+        start_index++;
+    }
+
+    for (int i = start_index; i < len_result; i++) {
+        printf("%c", digit_to_char(result[i]));
+    }
+
+    printf("\n");
+    free(result);
+}
+
+int main(int argc, char **argv) {
+	(void)argc;
+
+    multiply_strings(argv[1], argv[2]);
+
+    return 0;
 }
--- test.sh
+++ test.sh
@@ -6,7 +6,16 @@ echo "test 1: 1 * 1 = 1"
 ./a.out 1 1
 
 echo "test 2: 99 * 99 = 9801"
-./a.out 99 99
+./a.out '99' '99'
+
+echo "test 3: -99 * 99 = -9801"
+./a.out '-99' '99'
+
+echo "test 4: -99 * -99 = 9801"
+./a.out '-99' '-99'
+
+echo "test 5: 4582236412336 * 8822364411111111111 = 4.0426159e+31"
+./a.out 4582236412336 8822364411111111111
 
 # git add multiplier.c > /dev/null
 # git commit -m "commit" > /dev/null
